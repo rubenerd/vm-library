8d6841e478275a4b8a314b381ca6e04309f994c0
Ruben Schade <git@rubenschade.com>
2014-12-11 17:24:31 +1100

    removed crashing sb16 line, added isapc for networking

--- winnt-351-qemu.sh
+++ winnt-351-qemu.sh
@@ -8,16 +8,15 @@
 ## Notes:
 ## - KPs if post-486, or VT-x enabled (so don't accelerate)
 ## - AFAIK, initial FAT16 bootstrap doesn't like QCOW2
-##
-## TODO:
-## - Networking using isapc
+## - "soundhw sb16" crashes after login (sb16 not high priority any more):
+##     <http://bugs.launchpad.net/qemu/+bug/1295587>
 ##
 
 qemu-system-i386 \
+    -machine isapc \
     -m 16 \
     -cpu 486 \
     -k en-us \
-    -soundhw sb16 \
     -drive file=dos622.img,if=floppy,media=disk \
     -drive file=winnt-351.img,if=ide,index=0,media=disk \
     -drive file=winnt-351.iso,if=ide,index=2,media=cdrom
